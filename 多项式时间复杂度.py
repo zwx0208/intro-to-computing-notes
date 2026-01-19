e73c236c1339382d61b154ac58bd2f5602da2888
zwx0208 <2510305212@stu.pku.edu.cn>
2026-01-20 00:20:20 +0800

    Delete 多项式时间复杂度.py

--- 多项式时间复杂度.py
+++
@@ -1,7 +0,0 @@
-arr=list(input().strip().split('+'))
-flag=0
-for item in arr:
-    item_new=list(item.split("n^"))
-    if item_new[0]!='0' and int(item_new[1])>flag:
-        flag=int(item_new[1])
-print(f'n^{flag}')
