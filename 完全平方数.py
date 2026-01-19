8af5c63b4214fa6a2ff90a71c446af79dda8175f
zwx0208 <2510305212@stu.pku.edu.cn>
2026-01-20 00:07:54 +0800

    Delete 完全平方数.py

--- 完全平方数.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def numSquares(self, n: int) -> int:
-        nums = [x**2 for x in range(1, 101)]
-        dp=[float('inf')]*(n+1)
-        dp[0]=0
-        for num in nums:
-            for i in range(num,n+1):
-                dp[i]=min(dp[i],dp[i-num]+1)
-        return dp[n]
