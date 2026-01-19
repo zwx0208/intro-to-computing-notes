ee5ba3b26529c69a272de075e0721c2c821f5740
zwx0208 <2510305212@stu.pku.edu.cn>
2026-01-20 00:00:26 +0800

    Delete Xenia and Ringroad.py

--- Xenia and Ringroad.py
+++
@@ -1,9 +0,0 @@
-n,m=map(int,input().split())
-ringroad=list(map(int,input().split()))
-way=ringroad[0]-1
-for i in range(1,m):
-    if ringroad[i]>=ringroad[i-1]:
-        way+=ringroad[i]-ringroad[i-1]
-    elif ringroad[i]<ringroad[i-1]:
-        way+=n-ringroad[i-1]+ringroad[i]
-print(way)
