469d4b64ee9a0a20cdc5127ceee3f2f34ad0a563
zwx0208 <2510305212@stu.pku.edu.cn>
2026-01-20 00:04:27 +0800

    Delete 歌唱比赛.py

--- 歌唱比赛.py
+++
@@ -1,8 +0,0 @@
-n,m=map(int,input().split())
-con=[]
-for _ in range(n):
-    score=list(map(int,input().split()))
-    score.sort()
-    con.append(f'{sum(score[1:m-1])/(m-2):.2f}')
-con.sort()
-print(con[-1])
